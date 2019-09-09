0a50ed61c4d8585a8b85528ba8917c341d5afb49
Giovanni Aleksi Misitano <giovanni.a.misitano@student.jyu.fi>
2019-09-09 14:48:35 +0200

    Final version before demo.

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ xs = np.genfromtxt("./data/decision_result.csv", delimiter=",")
 fs = np.genfromtxt("./data/objective_result.csv", delimiter=",")
 objective_names = ["obj{}".format(i + 1) for i in range(fs.shape[1])]
 variable_names = ["x{}".format(i + 1) for i in range(xs.shape[1])]
-is_max = [True, True, False, False, False]
+is_max = [False, False, True, True, True]
 fs = np.where(is_max, -fs, fs)
 
 # scale the data
@@ -30,7 +30,7 @@ enautilus = ENautilus(problem)
 total_iters = 5
 points_shown = 4
 
-ideal, nadir = enautilus.initialize(total_iters, points_shown)
+nadir, ideal = enautilus.initialize(total_iters, points_shown)
 plotter = Plotter(nadir, ideal, scaler, is_max)
 
 # this is bad!
@@ -42,7 +42,9 @@ previous_best = None
 
 # fot the parallel axes
 columns, data = plotter.make_table(
-    zs=np.array([nadir, ideal]), names=objective_names, labels=["nadir", "ideal"]
+    zs=np.array([nadir, ideal]),
+    names=objective_names,
+    labels=["nadir", "ideal"],
 )
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
@@ -51,15 +53,18 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     [
+        # First row
         html.Div(
             [
                 html.H3(
                     "E-NAUTILUS: Iterations left {}".format(enautilus.ith - 1),
                     id="title",
+                    className="row",
                 ),
                 html.H4(
                     "Select the best candidate and iterate. "
-                    "Or just iterate if first iteration."
+                    "Or just iterate if first iteration.",
+                    className="six columns",
                 ),
                 dcc.RadioItems(
                     id="candidate-selection",
@@ -71,37 +76,78 @@ app.layout = html.Div(
                     ],
                     value=-1,
                     labelStyle={"display": "inline-block"},
+                    className="three columns",
                 ),
                 html.Button(
-                    id="iterate-button", n_clicks=0, children="ITERATE"
-                ),
-                dcc.Graph(
-                    id="spider-plots",
-                    figure=plotter.spider_plot_candidates(np.array([])),
+                    id="iterate-button",
+                    n_clicks=0,
+                    children="ITERATE",
+                    className="three columns",
                 ),
             ],
-            className="six columns",
+            className="row",
         ),
+        # Second row
         html.Div(
             [
-                html.H5("Value paths"),
-                dcc.Graph(
-                    id="value-paths",
-                    figure=plotter.value_path_plot_candidates(
-                        np.array([nadir, ideal]),
-                        objective_names,
-                        labels=["nadir", "ideal"],
-                    ),
+                # First column
+                html.Div(
+                    [
+                        html.H5("Spider plots"),
+                        dcc.Graph(
+                            id="spider-plots",
+                            figure=plotter.spider_plot_candidates(
+                                np.array([])
+                            ),
+                        ),
+                    ],
+                    className="six columns",
                 ),
-                html.H5("Tabled candidate objective values"),
-                dash_table.DataTable(id="table", columns=columns, data=data),
-                html.H5("Tabled candidate best reachable values"),
-                dash_table.DataTable(id="table-best"),
+                # Second column
+                html.Div(
+                    [
+                        html.H5(
+                            "Value paths (double click on first axis to show all paths)"
+                        ),
+                        dcc.Graph(
+                            id="value-paths",
+                            figure=plotter.value_path_plot_candidates(
+                                np.array([nadir, ideal]),
+                                objective_names,
+                                labels=["nadir", "ideal"],
+                            ),
+                        ),
+                    ],
+                    className="six columns",
+                ),
             ],
-            className="six columns",
+            className="row",
         ),
-    ],
-    className="row",
+        # Third row
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.H5("Tabled candidate objective values"),
+                        dash_table.DataTable(
+                            id="table",
+                            columns=columns,
+                            data=data,
+                        ),
+                    ],
+                    className="six columns",
+                ),
+                html.Div(
+                    [
+                        html.H5("Tabled candidate best reachable values"),
+                        dash_table.DataTable(id="table-best"),
+                    ],
+                    className="six columns",
+                ),
+            ],
+            className="row",
+        ),
+    ]
 )
 
 
@@ -109,10 +155,20 @@ app.layout = html.Div(
     [
         Output("table", "style_data_conditional"),
         Output("table-best", "style_data_conditional"),
+        Output("spider-plots", "figure"),
+        Output("value-paths", "figure"),
     ],
     [Input("candidate-selection", "value")],
 )
 def highlight_table_row(candidate_index):
+    global intermediate_points
+    global intermediate_ranges
+    global current_best_idx
+    global previous_best
+
+    if candidate_index == -1:
+        raise PreventUpdate
+
     style = [
         {
             "if": {"row_index": candidate_index},
@@ -121,19 +177,35 @@ def highlight_table_row(candidate_index):
         }
     ]
 
-    return style, style
+    if len(intermediate_points) == 0:
+        zs = np.array([])
+    else:
+        zs = intermediate_points
+
+    spider_plots = plotter.spider_plot_candidates(
+        zs,
+        names=objective_names,
+        best=intermediate_ranges,
+        previous=previous_best,
+        selection=candidate_index,
+    )
+
+    value_paths = plotter.value_path_plot_candidates(
+        zs, objective_names, selection=candidate_index
+    )
+
+    return style, style, spider_plots, value_paths
 
 
 @app.callback(
     [
-        Output("spider-plots", "figure"),
         Output("candidate-selection", "options"),
         Output("title", "children"),
         Output("table", "columns"),
         Output("table", "data"),
         Output("table-best", "columns"),
         Output("table-best", "data"),
-        Output("value-paths", "figure"),
+        Output("candidate-selection", "value"),
     ],
     [Input("iterate-button", "n_clicks")],
     [State("candidate-selection", "value")],
@@ -147,9 +219,10 @@ def update_candidates(n_clicks, candidate_index):
     if n_clicks == 0:
         raise PreventUpdate
     if n_clicks == 1:
-        # first iteratino, do not interact
+        # first iteration, do not interact
         zs, best = enautilus.iterate()
     else:
+        previous_best = intermediate_points[current_best_idx]
         current_best_idx = candidate_index
         enautilus.interact(
             intermediate_points[current_best_idx],
@@ -159,15 +232,6 @@ def update_candidates(n_clicks, candidate_index):
 
     intermediate_points = zs
     intermediate_ranges = best
-
-    spider_plots = plotter.spider_plot_candidates(
-        zs,
-        names=objective_names,
-        best=intermediate_ranges,
-        previous=previous_best,
-    )
-
-    previous_best = zs[current_best_idx]
 
     options = [
         {"label": "Candidate {}".format(ind + 1), "value": val}
@@ -184,18 +248,7 @@ def update_candidates(n_clicks, candidate_index):
         zs=best, names=objective_names, row_name=["Best reachable"]
     )
 
-    value_paths = plotter.value_path_plot_candidates(zs, objective_names)
-
-    return (
-        spider_plots,
-        options,
-        title,
-        columns,
-        data,
-        columns_best,
-        data_best,
-        value_paths,
-    )
+    return (options, title, columns, data, columns_best, data_best, 0)
 
 
 def main():
--- desdeo_dash/plotting.py
+++ desdeo_dash/plotting.py
@@ -3,6 +3,10 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from typing import List, Optional, Tuple
 from plotly.graph_objs._figure import Figure
+import itertools
+import copy
+
+
 import pandas as pd
 
 
@@ -23,6 +27,22 @@ class Plotter:
         The nadir and ideal are assumed to be already normalized.
     """
 
+    # always deepcopy this to get consistent colors
+    colors = itertools.cycle(
+        [
+            "#1f77b4",  # muted blue
+            "#ff7f0e",  # safety orange
+            "#2ca02c",  # cooked asparagus green
+            "#d62728",  # brick red
+            "#9467bd",  # muted purple
+            "#8c564b",  # chestnut brown
+            "#e377c2",  # raspberry yogurt pink
+            "#7f7f7f",  # middle gray
+            "#bcbd22",  # curry yellow-green
+            "#17becf",  # blue-teal
+        ]
+    )
+
     def __init__(
         self,
         nadir: np.ndarray,
@@ -35,7 +55,7 @@ class Plotter:
         self.scaler = scaler
         self.is_max = is_max
 
-    def spider_plot_candidates(
+    def _spider_plot_candidates(
         self,
         zs: np.ndarray,
         names: Optional[List[str]] = None,
@@ -113,9 +133,9 @@ class Plotter:
             subplot_titles=titles,
         )
 
-        fig["layout"]["width"] = cols * 225
-        fig["layout"]["height"] = rows * 225
-        fig["layout"]["autosize"] = True
+        # fig["layout"]["width"] = cols * 225
+        # fig["layout"]["height"] = rows * 225
+        # fig["layout"]["autosize"] = True
         polars = ["polar"] + [
             "polar{}".format(i + 1) for i in range(1, len(zs))
         ]
@@ -213,7 +233,7 @@ class Plotter:
 
         return fig
 
-    def value_path_plot_candidates(
+    def _value_path_plot_candidates(
         self,
         zs: np.ndarray,
         names: Optional[List[str]] = None,
@@ -327,7 +347,222 @@ class Plotter:
         if labels:
             df["Candidate"] = labels
 
+        df.style.format("{:.2f%}")
         columns = [{"name": i, "id": i} for i in df.columns]
         data = df.to_dict("records")
 
+        # Reformat floats to only show two decimal places. This is slow if a lot of data.
+        for entry in data:
+            for key in entry:
+                if key == row_name[0]:
+                    continue
+                entry[key] = "{0:.2f}".format(entry[key])
+
         return columns, data
+
+    def spider_plot_candidates(
+        self,
+        zs: np.ndarray,
+        names: Optional[List[str]] = None,
+        labels: Optional[List[str]] = None,
+        best: Optional[np.ndarray] = None,
+        previous: Optional[np.ndarray] = None,
+        selection: Optional[int] = None,
+    ) -> Figure:
+        """Plots multiple solution candiates with the best reachable values and
+        the previous candidate chosen shown, if defined.
+
+        Args:
+            zs (np.ndarray): A 2D array with each candidate on its' rows.
+            names (List[str], optional): A list of the objective names.
+            labels (List[str], optioanl): A list of the solution names.
+            best (np.ndarray, optional): A 2D array with the best reachable
+            solution from each candidate on its' rows.
+            previous (np.ndarray, optional): An array with the previously
+            chosen candidate.
+            dims (Tuple[int, int], optional): How many solutions should be
+            displayed on each row and column
+
+        """
+        if zs.size == 0:
+            # make empty plot (for placeholding purposes)
+            fig = make_subplots(specs=[[{"type": "polar"}]])
+            fig["layout"]["autosize"] = True
+            return {}
+
+        if zs.ndim == 1:
+            # reshape single solution to function as 2D array
+            zs = zs.reshape(1, -1)
+
+        zs_original = self.scaler.inverse_transform(zs)
+        zs_original = np.where(self.is_max, -zs_original, zs_original)
+        zs = np.where(self.is_max, -zs, zs)
+
+        if names is None:
+            names = ["Obj {}".format(i + 1) for i in range(zs.shape[1])]
+        names = [
+            "{} ({})".format(name, val)
+            for (val, name) in zip(
+                ["max" if m is True else "min" for m in self.is_max], names
+            )
+        ]
+
+        if best is not None and best.ndim == 1:
+            best = best.reshape(1, -1)
+        if best is not None:
+            best_original = self.scaler.inverse_transform(best)
+            best_original = np.where(
+                self.is_max, -best_original, best_original
+            )
+            best = np.where(self.is_max, -best, best)
+
+        if previous is not None and previous.ndim == 1:
+            previous = previous.reshape(1, -1)
+        if previous is not None:
+            previous_original = self.scaler.inverse_transform(previous)
+            previous_original = np.where(
+                self.is_max, -previous_original, previous_original
+            )
+            previous = np.where(self.is_max, -previous, previous)
+
+        if labels is None:
+            titles = ["Candidate {}".format(i + 1) for i in range(zs.shape[1])]
+        else:
+            titles = labels
+
+        fig = go.Figure()
+
+        colors_best = copy.deepcopy(Plotter.colors)
+        if best is not None:
+            for (b_i, b) in enumerate(best):
+                if b_i == selection:
+                    vis = True
+                else:
+                    vis = "legendonly"
+
+                fig.add_trace(
+                    go.Scatterpolar(
+                        r=b,
+                        meta=best_original[b_i],
+                        theta=names,
+                        fill="none",
+                        opacity=0.75,
+                        name=titles[b_i] + " best",
+                        legendgroup=str(b_i),
+                        visible=vis,
+                        hovertemplate="%{theta}: %{meta:.2f}",
+                        line={"color": next(colors_best), "dash": "dot"},
+                    )
+                )
+
+        if previous is not None:
+            fig.add_trace(
+                go.Scatterpolar(
+                    r=previous[0],
+                    meta=previous_original[0],
+                    theta=names,
+                    fill="none",
+                    name="Previous candidate",
+                    hovertemplate="%{theta}: %{meta:.2f}",
+                    line={"color": "black", "dash": "dot"},
+                )
+            )
+
+        colors_candidate = copy.deepcopy(Plotter.colors)
+        for (z_i, z) in enumerate(zs):
+            if z_i == selection:
+                vis = True
+            else:
+                vis = "legendonly"
+
+            fig.add_trace(
+                go.Scatterpolar(
+                    r=z,
+                    meta=zs_original[z_i],
+                    theta=names,
+                    fill="none",
+                    legendgroup=str(z_i),
+                    visible=vis,
+                    name=titles[z_i],
+                    hovertemplate="%{theta}: %{meta:.2f}",
+                    line={"color": next(colors_candidate)},
+                )
+            )
+
+        fig.update_layout(
+            polar=dict(radialaxis=dict(visible=False, range=[-1, 1]))
+        )
+
+        return fig
+
+    def value_path_plot_candidates(
+        self,
+        zs: np.ndarray,
+        names: Optional[List[str]] = None,
+        labels: Optional[List[str]] = None,
+        selection: Optional[int] = None,
+    ) -> Figure:
+        """Plots multiple solution candidates as an parallel axis plot.
+
+        Args:
+            zs (np.ndarray): A 2D array with each candidate on its' rows.
+            names (List[str], optional): A list of the objective names.
+        """
+        if zs.ndim == 1:
+            zs = zs.reshape(1, -1)
+        zs_original = self.scaler.inverse_transform(zs)
+        zs_original = np.where(self.is_max, -zs_original, zs_original)
+
+        nadir_original = self.scaler.inverse_transform(
+            self.nadir.reshape(1, -1)
+        )[0]
+        nadir_original = np.where(self.is_max, -nadir_original, nadir_original)
+
+        ideal_original = self.scaler.inverse_transform(
+            self.ideal.reshape(1, -1)
+        )[0]
+        ideal_original = np.where(self.is_max, -ideal_original, ideal_original)
+
+        if names is None:
+            names = ["Obj {}".format(i + 1) for i in range(zs.shape[1])]
+
+        names = [
+            "{} ({})".format(name, val)
+            for (val, name) in zip(
+                ["max" if m is True else "min" for m in self.is_max], names
+            )
+        ]
+
+        lows = []
+        ups = []
+
+        for i in range(len(nadir_original)):
+            if nadir_original[i] < ideal_original[i]:
+                lows.append(nadir_original[i])
+                ups.append(ideal_original[i])
+            else:
+                ups.append(nadir_original[i])
+                lows.append(ideal_original[i])
+
+        rows = [list(row) for row in zs_original.T]
+        fig = go.Figure(
+            data=go.Parcoords(
+                line=dict(color=list(range(len(rows))), showscale=True),
+                dimensions=list(
+                    [
+                        dict(range=[low, up], label=name, values=vals)
+                        for (low, up, name, vals) in zip(
+                            lows, ups, names, rows
+                        )
+                    ]
+                ),
+            )
+        )
+
+        if selection is not None:
+            fig["data"][0]["dimensions"][0]["constraintrange"] = [
+                zs_original[selection][0] - 1,
+                zs_original[selection][0] + 1,
+            ]
+
+        return fig
