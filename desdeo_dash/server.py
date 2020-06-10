9e6618590b0f16f13eee4687e3204de08aece92b
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2020-06-10 23:45:22 +0300

    Added assets folder.

--- desdeo_dash/server.py
+++ desdeo_dash/server.py
@@ -1,8 +1,9 @@
 import dash
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+assets_folder = "./assets"
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, assets_folder=assets_folder)
 app.title = "Multiobjective optimization with NAUTILUS"
 app.config.suppress_callback_exceptions = True
 server = app.server
